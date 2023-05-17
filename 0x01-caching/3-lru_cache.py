2e7f5923688f3c58d498697a2aada32ed6fd2bf6
Sn0wF1re <mokayaclinton34@gmail.com>
2023-05-17 16:28:39 +0300

    Overhaul the solution

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -2,41 +2,47 @@
 """
 Create class LRUCache
 """
+from collections import OrderedDict
+
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
     """
-    Inherits from BaseCaching and is a caching system
+    Implements LRU cache replacement policy
     """
     def __init__(self):
         """
-        Initializes the class
+        Initialize super class
         """
         super().__init__()
-        self.accessed = []
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """
         assign to the dictionary self.cache_data the item value
         for the key key
         """
-        if key and item:
-            length = len(self.cache_data)
-            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print(f"DISCARD: {self.accessed[0]}")
-                self.cache_data.pop(self.accessed[0])
-                del self.accessed[0]
+        if key is None or item is None:
+            return
 
-            if key in self.accessed:
-                self.accessed.remove(key)
-            self.accessed.append(key)
-            self.cache_data[key] = item
+        if key in self.cache_data:
+            self.cache_data.pop(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            lru_key = next(iter(self.cache_data))
+            print(f"DISCARD: {lru_key}")
+            self.cache_data.pop(lru_key)
+
+        self.cache_data[key] = item
 
     def get(self, key):
         """
-        return the value in self.cache_data linked to key
+        Return value in self.cache_data linked to key
         """
-        if key in self.cache_data and key is not None:
-            return self.cache_data[key]
+        if key in self.cache_data:
+            val = self.cache_data[key]
+            self.cache_data.pop(key)
+            self.cache_data[key] = val
+            return val
+
         return None
