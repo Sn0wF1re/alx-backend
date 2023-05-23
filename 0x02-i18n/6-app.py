68e0e8c05981708e335089c7d66f7c234b771de7
Sn0wF1re <mokayaclinton34@gmail.com>
2023-05-24 00:05:28 +0300

    Change get_locale function to use a user’s preferred local if it is supported

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -9,14 +9,10 @@ from flask import (
     g
 )
 from flask_babel import Babel
-
-
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
+from typing import (
+    Dict,
+    Union
+)
 
 
 class Config(object):
@@ -33,14 +29,22 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-def get_user():
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[Dict, None]:
     """
     returns a user dictionary
     or None if the ID cannot be found
     or if login_as was not passed
     """
     id = request.args.get('login_as', None)
-    if id is not None and int(id) in users.keys():
+    if id and int(id) in users.keys():
         return users.get(int(id))
     return None
 
@@ -62,15 +66,22 @@ def get_locale():
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
         return locale
+    if g.user:
+        locale = g.user.get('locale')
+        if locale and locale in app.config['LANGUAGES']:
+            return locale
+    locale = request.headers.get('locale', None)
+    if locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """
-    Render index template
+    render index template
     """
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == "__main__":
