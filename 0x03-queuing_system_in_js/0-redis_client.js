a9720c4a2448d237dac42b7ca97e3e2e2583d84c
Sn0wF1re <mokayaclinton34@gmail.com>
2023-06-20 12:14:19 +0300

    Use Node Redis client for basic operations

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -3,7 +3,7 @@ import { createClient } from 'redis';
 const client = createClient();
 
 client.on('connect', () => {
-    console.log('Redis client connected to the server');
+  console.log('Redis client connected to the server');
 }).on('error', (err) => {
-    console.log(`Redis client not connected to the server: ${err}`);
+  console.log(`Redis client not connected to the server: ${err}`);
 });
---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,28 @@
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+}).on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, print);
+};
+
+const displaySchoolValue = (schoolName) => {
+  client.get(schoolName, (err, res) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    console.log(res);
+  });
+};
+
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
